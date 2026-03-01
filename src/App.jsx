aaa375125305a244cc7fabd35db18dcd1819596c
KrishDevCrafting <krishkumer150@gmail.com>
2026-03-01 22:28:52 +0530

    feat: initialize Skill section
    - Created Skill.jsx component
    - Added Skill to App.jsx routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/Layout/Navbar";
 import Hero from "./components/Section/Hero";
 import About from "./components/Section/About";
+import Skill from "./components/Section/Skill";
 const App = () => {
   return (
     <div className="min-h-screen bg-black pb-[100vh]">
@@ -9,6 +10,7 @@ const App = () => {
       <main>
         <Hero />
         <About />
+        <Skill/>
       </main>
     </div>
   );
